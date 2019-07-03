d69520fe1e7bd4bc26f0a92c31aef42f10188fda
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-07-03 20:01:42 -0300

    cria cabeçalho

--- tuquersabermais/src/screens/index.js
+++ tuquersabermais/src/screens/index.js
@@ -3,26 +3,128 @@ import {
     Text,
     View,
     Image,
-    StyleSheet
+    StyleSheet,
+    TouchableOpacity,
 } from 'react-native';
 
+const screens = [
+    {
+        key: 0,
+        description: 'Playlist',
+        active: true
+    },
+
+    {
+        key: 1,
+        description: 'About',
+        active: false
+    },
+]
+
 export default class Preload extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            page: 0,
+        }
+    }
+
+    screensTab(){
+        return screens.map((tab) =>{
+            return (
+                <View key={tab.key}>
+                    <TouchableOpacity style={styles.tabItem} onPress={() => this.changeTab(tab.key)}>
+                        <Text style={styles.tab}>{tab.description}</Text>
+                        <View
+                            style={{
+                                height: 1,
+                                backgroundColor: (this.state.page == tab.key ? '#FFF' : 'transparent')
+                            }}
+                        />
+                    </TouchableOpacity>
+                </View>    
+            );
+        })
+    }
+
+    changeTab(tab){
+        this.viewPager.setPage(tab);
+        this.setState({page : tab});
+    }
+
     render(){
         return(
             <View style={styles.container}>
-                <Text>index - screens</Text>
+                <View style={styles.header}>
+                    <Image
+                        style={styles.logo}
+                        source={require('../assets/logo.png')}
+                    />
+                    <View style={styles.desc}>
+                        <Text style={styles.title}>Wagner Lorenz</Text>
+
+                        <View style={styles.options}>
+                            {this.screensTab()}
+                        </View>
+                    </View>
+                </View>
+
+                
             </View>
         );
     }
 }
 
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
 
         backgroundColor: '#575757'
+    },
+
+    header: {
+        margin: 10,
+        alignSelf: 'center',
+
+        flexDirection: 'row',
+    },
+
+    logo: {
+        width: 100,
+        height: 100,
+    },
+
+    desc: {
+        marginLeft: 15,
+    },
+
+    title: {
+        color: '#FFF',
+        fontFamily: 'sans-serif-light',
+        fontSize: 30,
+    },
+
+    options: {
+        flexDirection: 'row',
+    },
+
+    tabLines: {
+        flexDirection: 'row',
+    },
+
+    tabItem: {
+        marginRight: 7,
+    },
+
+    tab: {
+        color: '#FFF',
+        fontFamily: 'sans-serif-light',
+        fontSize: 16,
+    },
+
+    viewPager: {
+        flex: 1,
     },
 });
